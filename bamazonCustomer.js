57f9396d425c58f0dbe4b7d900e7531fb910b56b
Mrrwmix <seidoc@gmail.com>
2019-07-18 21:16:50 -0700

    bamazonCustomer can SELECT and UPDATE now

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,7 +21,64 @@ connection.connect(function(err) {
       itemList.push(res[x].product_name);
     }
     items = res;
-    console.log(Table.print(res));
   });
-  connection.end();
+  connection.query('SELECT product_name, price FROM products', function(
+    err,
+    res
+  ) {
+    if (err) throw err;
+    console.log(Table.print(res));
+    inquire
+      .prompt([
+        {
+          name: 'theChoice',
+          type: 'list',
+          message: 'Which item would you like to purchase?',
+          choices: itemList
+        }
+      ])
+      .then(function(answer) {
+        inquire
+          .prompt([
+            {
+              name: 'quantity',
+              type: 'input',
+              message: 'You chose ' + answer.theChoice + '. Quantity needed?'
+            }
+          ])
+          .then(function(amount) {
+            for (y in items) {
+              if (items[y].product_name == answer.theChoice) {
+                if (items[y].stock_quantity < amount.quantity) {
+                  console.log('Insufficient quantity!');
+                } else {
+                  connection.query(
+                    'UPDATE products SET stock_quantity=' +
+                      (parseInt(items[y].stock_quantity) -
+                        parseInt(amount.quantity)) +
+                      " WHERE product_name='" +
+                      answer.theChoice +
+                      "'",
+                    function(err) {
+                      if (err) throw err;
+                    }
+                  );
+                  console.log(
+                    'Successfully purchased ' +
+                      answer.theChoice +
+                      ' (Quantity: ' +
+                      amount.quantity +
+                      ') for ' +
+                      parseFloat(
+                        parseFloat(items[y].price) * parseInt(amount.quantity)
+                      ) +
+                      '!\nThanks for shopping on Bamazon.'
+                  );
+                }
+              }
+            }
+            connection.end();
+          });
+      });
+  });
 });
