533638b93f7773865b766a8dbcbc36399088cf64
Mrrwmix <seidoc@gmail.com>
2019-07-18 17:59:46 -0700

    Super duper table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 const inquire = require('inquirer');
 const mysql = require('mysql');
-const table = require('table');
+const Table = require('easy-table');
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -8,4 +8,20 @@ var connection = mysql.createConnection({
   user: 'root',
   password: 'pword',
   database: 'bamazon'
+});
+
+var itemList = [];
+var items;
+
+connection.connect(function(err) {
+  if (err) throw err;
+  connection.query('SELECT * FROM products', function(err, res) {
+    if (err) throw err;
+    for (x in res) {
+      itemList.push(res[x].product_name);
+    }
+    items = res;
+    console.log(Table.print(res));
+  });
+  connection.end();
 });
